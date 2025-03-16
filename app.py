26d4ceed7e46552996bf60e4c33a1d01cfcdd7d6
KrystianKoziejaWrk <KrystianKoziejaWrk@gmail.com>
2025-03-16 17:13:50 -0500

    Please work

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello")
+from flask import Flask, jsonify, request, send_from_directory, render_template, url_for, redirect, session, request, flash
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.secret_key
+
+
+if __name__ == "__main__":
+    app.run()
