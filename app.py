d2d9f275dbfb261facf7a3f326369b60695c914f
KrystianKoziejaWrk <KrystianKoziejaWrk@gmail.com>
2025-03-16 22:11:15 -0500

    Setting up database tables

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request, send_from_directory, render_template, url_for, redirect, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-
 # Google auth
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
@@ -13,6 +12,9 @@ app = Flask(__name__)
 # Configure the database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"  # SQLite for local testing
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
 
 # Initialize the database
 db.init_app(app)
