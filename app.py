1b177341efdcb7f49f7d85f9268f2bdf6c16703c
KrystianKoziejaWrk <KrystianKoziejaWrk@gmail.com>
2025-03-17 00:43:32 -0500

    Bro changing app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, request, send_from_directory, render_template, url_for, redirect, flash
+from flask import Flask, jsonify, request, send_from_directory, render_template, url_for, redirect, flash, session
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 # Google auth
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 
-from models import db, Users
+from models import db, Users, FocusSession
 
 app = Flask(__name__)
 
@@ -13,14 +13,36 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"  # SQLite for local testing
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
-
 
 # Initialize the database
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+#Creating the approutes for logging in and logging out
+@app.route("/signup", methods=["POST","GET"])
+def signup():
+    if request.method == "POST":
+
+        session.permanent = True
+
+        user_name = request.form["user_name_input"]
+        session["user_name"] = user_name
+
+        user_email = request.form["user_email_input"]
+        session["user_email"] = user_email
+
+        user_password = request.form["user_password_input"]
+        session["user_password"] = user_email
+
+        
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
